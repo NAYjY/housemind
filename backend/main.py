12e2cde6284f1244c3bd6815bff2f387def1b7d3
NAYjY <nckcvckforwork@gmail.com>
2026-05-11 14:00:20 +0700

    CORS wildcard methods with credentials

--- backend/main.py
+++ backend/main.py
@@ -103,8 +103,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.cors_origins_list,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allow_headers=["Authorization", "Content-Type", "X-Request-ID", "X-Health-Secret"],
+    expose_headers=["X-Request-ID"],
+    max_age=600,
 )
 app.add_middleware(RequestLoggingMiddleware)
 
